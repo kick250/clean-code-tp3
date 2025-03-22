dd8ada7f70002048f0e8ea96c24885a87c1e2765
kick250 <kicklobat@gmail.com>
2025-03-22 06:42:55 -0300

    start commit

---
+++ src/test/java/com/projectsManager/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsManager.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
